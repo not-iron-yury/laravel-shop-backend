9902f57ca40511231af7617e7e1c068139b16a2e
not-iron-yury <not.iron.yuriy@inbox.ru>
2025-06-24 18:35:22 +0300

    Добавлен Product
    new file:   app/Models/Product.php
    modified:   database/migrations/2025_06_24_123515_create_products_table.php

--- database/migrations/2025_06_24_123515_create_products_table.php
+++ database/migrations/2025_06_24_123515_create_products_table.php
@@ -13,6 +13,29 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            /**
+             * $table->foreignId('user_id')
+             * создаёт поле user_id типа BIGINT UNSIGNED
+             * внешняя связь (foreign key) — на пользователя
+             *
+             * ->constrained()
+             * Laravel автоматически добавляет внешнюю связь на таблицу users(id)
+             * сокращение $table->foreign('user_id')->references('id')->on('users');
+             * Работает по имени поля — user_id → значит связь с users
+             *
+             * ->onDelete('cascade')
+             * каскадное удаление
+             * если user будет удалён, то все его товары удалятся автоматически
+             * для комментариев - да, для товаров - не стоит.
+             *
+             */
+            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained();
+
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
