5247775000e374293b18b9229ac8eb773491db96
not-iron-yury <not.iron.yuriy@inbox.ru>
2025-06-24 13:25:14 +0300

    Создан Gate isAdmin
    Правило 'isAdmin' в app/Providers/AppServiceProvider.php
    Тестовый маршрут в routes/api.php с проверкой соответствия правилу.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /**
+         * define — регистрирует правило.
+         * В данном случае регистрируем правило 'isAdmin' и логику (анонимную функию) для него
+         */
+        Gate::define('isAdmin', function($user){
+            return $user->role === 'admin';
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use Illuminate\Support\Facades\Gate;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -17,3 +17,13 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->get('/admin/test', function() {
+
+    // проверка на несоответствие правилу 'isAdmin'
+    if(Gate::denies('isAdmin')){
+        abort(403, 'Доступ запрещен.');
+    }
+
+    return 'Добро пожаловать в админку';
+});
