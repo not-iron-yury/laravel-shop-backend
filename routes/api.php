b49931cf1529d37185eed8ecae4c0f22682ef515
not-iron-yury <not.iron.yuriy@inbox.ru>
2025-06-24 20:29:11 +0300

    Роуты для products (весь набор запросов)

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Gate;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-// /user больше не нужен — теперь его роль выполняет /me, но с контроллером.
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
-});
-
+// Доступно без авторизации
+// register и login выполняются до авторизации (пользователь ещё не имеет токена).
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
-Route::middleware(['auth:sanctum'])->get('/admin/test', function() {
+// Только для авторизованных пользователей
+// Middleware auth:sanctum требует уже авторизованного пользователя (с действующим токеном).
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::apiResource('products', ProductController::class);
+});
 
+Route::middleware(['auth:sanctum'])->get('/admin/test', function() {
     // проверка на несоответствие правилу 'isAdmin'
     if(Gate::denies('isAdmin')){
         abort(403, 'Доступ запрещен.');
